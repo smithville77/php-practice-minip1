f5290acffe7629ebd6f0c17bce072cdf08a95105
Adam Smith <volcom_stone664@hotmail.com>
2024-02-16 21:30:00 +1100

    added small amount of content

---
+++ src/DB/PostDB.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\DB;
+
+class PostDB
+{
+    public static $dataset = [
+        [
+          "title" => "How to create a php file structure",
+          "date" => "16-02-2024",
+          "user" => "Smith",
+          "blogContent" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut cupiditate, beatae vel odio modi suscipit eveniet ipsa at in et!",
+          "tags" => ["PHP", "Programming", "File Structure"]
+        ],
+        [
+          "title" => "How to train your dragon",
+          "date" => "17-02-2024",
+          "user" => "Smith",
+          "blogContent" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut cupiditate, beatae vel odio modi suscipit eveniet ipsa at in et!",
+          "tags" => ["PHP", "Programming", "File Structure", "dragons"]
+        ],
+        [
+          "title" => "Another spiffy title",
+          "date" => "18-02-2024",
+          "user" => "Smith",
+          "blogContent" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut cupiditate, beatae vel odio modi suscipit eveniet ipsa at in et!",
+          "tags" => ["PHP", "Programming", "File Structure"]
+        ],
+        [
+          "title" => "How to create a php file structure pt.2 ",
+          "date" => "20-02-2024",
+          "user" => "Smith",
+          "blogContent" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut cupiditate, beatae vel odio modi suscipit eveniet ipsa at in et!",
+          "tags" => ["PHP", "Programming", "File Structure"]
+        ]
+        ];
+}
+
+?>
---
+++ src/Pages/ViewPosts.php
@@ -0,0 +1,30 @@
+<?php
+require __DIR__ . '/../../vendor/autoload.php';
+
+use App\DataAccess\BlogRepository;
+
+$blogRepository = new BlogRepository();
+$blogPosts = $blogRepository->getAllPosts();
+
+?>
+
+<h1>Welcome to the blog about nothing!</h1>
+<div>
+    <?php foreach ($blogPosts as $post) : ?>
+        <div class="blog-post">
+            <div class="blog-title">Title: <?php echo $post->title ?></div>
+            <div class="blog-date">Date: <?php echo $post->date ?></div>
+            <div class="username">User: <?php echo $post->user ?></div>
+            <div class="blog-content"><?php echo $post->blogContent ?></div>
+            <div class="blog-tags">
+                <?php foreach ($post->tags as $tag) : ?>
+                    <span class="tag">Tags: <?php echo $tag; ?></span>
+                <?php endforeach ?>
+            </div>
+            <br />
+        </div>
+
+
+    <?php endforeach ?>
+</div>
+
