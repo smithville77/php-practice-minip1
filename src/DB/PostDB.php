19601b37ff1964620e1d2761527931a136cf5582
Adam Smith <volcom_stone664@hotmail.com>
2024-02-17 15:26:48 +1100

    styling

--- src/DB/PostDB.php
+++ src/DB/PostDB.php
@@ -7,34 +7,32 @@ class PostDB
 {
     public static $dataset = [
         [
-          "title" => "How to create a php file structure",
-          "date" => "16-02-2024",
-          "user" => "Smith",
-          "blogContent" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut cupiditate, beatae vel odio modi suscipit eveniet ipsa at in et!",
-          "tags" => ["PHP", "Programming", "File Structure"]
+            "title" => "How to create a php file structure",
+            "date" => "16-02-2024",
+            "user" => "Smith",
+            "blogContent" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut cupiditate, beatae vel odio modi suscipit eveniet ipsa at in et!",
+            "tags" => ["PHP", "Programming", "File Structure"]
         ],
         [
-          "title" => "How to train your dragon",
-          "date" => "17-02-2024",
-          "user" => "Smith",
-          "blogContent" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sint rem expedita numquam porro perspiciatis. Incidunt quod optio esse perspiciatis dicta. Architecto, eveniet nulla ipsum id fugiat illum aperiam a, obcaecati illo cumque labore ad accusamus reprehenderit. Magni enim distinctio quam iste deserunt, ipsam qui, minus, at eligendi ipsa eveniet.",
-          "tags" => ["PHP", "Programming", "File Structure", "dragons"]
+            "title" => "How to train your dragon",
+            "date" => "17-02-2024",
+            "user" => "Smith",
+            "blogContent" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sint rem expedita numquam porro perspiciatis. Incidunt quod optio esse perspiciatis dicta. Architecto, eveniet nulla ipsum id fugiat illum aperiam a, obcaecati illo cumque labore ad accusamus reprehenderit. Magni enim distinctio quam iste deserunt, ipsam qui, minus, at eligendi ipsa eveniet.",
+            "tags" => ["PHP", "Programming", "File Structure", "dragons"]
         ],
         [
-          "title" => "Another spiffy title",
-          "date" => "18-02-2024",
-          "user" => "Smith",
-          "blogContent" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut cupiditate, beatae vel odio modi suscipit eveniet ipsa at in et!",
-          "tags" => ["PHP", "Programming", "File Structure"]
+            "title" => "Another spiffy title",
+            "date" => "18-02-2024",
+            "user" => "Smith",
+            "blogContent" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut cupiditate, beatae vel odio modi suscipit eveniet ipsa at in et!",
+            "tags" => ["PHP", "Programming", "File Structure"]
         ],
         [
-          "title" => "How to create a php file structure pt.2 ",
-          "date" => "20-02-2024",
-          "user" => "Smith",
-          "blogContent" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut cupiditate, beatae vel odio modi suscipit eveniet ipsa at in et!",
-          "tags" => ["PHP", "Programming", "File Structure"]
+            "title" => "How to create a php file structure pt.2 ",
+            "date" => "20-02-2024",
+            "user" => "Smith",
+            "blogContent" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut cupiditate, beatae vel odio modi suscipit eveniet ipsa at in et!",
+            "tags" => ["PHP", "Programming", "File Structure"]
         ]
-        ];
+    ];
 }
-
-?>
