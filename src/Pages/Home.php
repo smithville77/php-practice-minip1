0dcfd1e9c4d293a2f473eaf8dd393161bcb61f87
Adam Smith <volcom_stone664@hotmail.com>
2024-02-17 15:25:58 +1100

    added styling to a basic home page

--- src/Pages/Home.php
+++ src/Pages/Home.php
@@ -1,12 +1,38 @@
 <?php
-// include the header file
+require __DIR__ . '/../../vendor/autoload.php';
+
+use App\DataAccess\BlogRepository;
+
+$blogRepository = new BlogRepository();
+$blogPosts = $blogRepository->getAllPosts();
+
+
 require_once __DIR__ . '/../SharedComponents/Header.php';
 ?>
 
 
-<container class="container">
-    <h1>Home page</h1>
-</container>
+<div class="container">
+    <h1 class="mb-4 mt-5">Welcome</h1>
+    <container class="row">
+        <div class="col-8">
+            <h2>News</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat atque enim consectetur libero earum, cumque odit esse quas praesentium sequi ex rem laboriosam a vero, sapiente repellat? Delectus explicabo officia, accusantium ducimus aspernatur tempora praesentium temporibus, tenetur aut consectetur laborum?</p>
+        </div>
+        <div class="col-4">
+            <h4>Recent Posts</h4>
+            <?php foreach ($blogPosts as $post) : ?>
+                <div class="border-top border-bottom border-secondary p-2 mb-3">
+                    <h6><?php echo $post->title ?></h6>
+                <p><?php echo substr($post->blogContent, 0, 50) . "..." ?></p>
+                <button class="btn btn-primary">See more</button>
+                </div>
+                
+            <?php endforeach ?>
+
+            <button class="btn btn-secondary "><a class="text-white text-decoration-none" href="/php-practice-minip1/src/Pages/ViewPosts.php">See all posts</a></button>
+        </div>
+    </container>
+</div>
 
 <?php
 // include the header file
