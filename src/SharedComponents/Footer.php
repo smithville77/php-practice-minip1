659ac020085ed44957a4ae8115b5c93af6c3c6c6
Adam Smith <volcom_stone664@hotmail.com>
2024-02-18 11:35:34 +1100

    added completely out of context sociail icons to the footer

--- src/SharedComponents/Footer.php
+++ src/SharedComponents/Footer.php
@@ -9,15 +9,29 @@ $currentYear = $today["year"];
 <footer>
 
 
-<div class="container">
-  <footer class="py-3 my-4">
-    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
-      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
-    </ul>
-    <p class="text-center text-muted">© MyBlog inc, <?php echo $currentYear ?> </p>
-  </footer>
-</div>
+    <div class="container">
+        <footer class="py-3 my-4">
+            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+            </ul>
+
+            <div class=" d-flex justify-content-center p-2">
+                <p class="text-center text-muted">© MyBlog inc, <?php echo $currentYear ?> </p>
+                <div class="ms-3">
+                    <span class="material-symbols-outlined">
+                        sentiment_stressed
+                    </span>
+                    <span class="material-symbols-outlined">
+                        ophthalmology
+                    </span>
+                    <span class="material-symbols-outlined">
+                        water_do
+                    </span>
+                </div>
+            </div>
+        </footer>
+    </div>
 </footer>
 </body>
 
