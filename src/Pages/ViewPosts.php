44b6accdd33ef98168febb520490e449951a2384
Adam Smith <volcom_stone664@hotmail.com>
2024-02-18 11:50:16 +1100

    changed tag color & text-decoration

--- src/Pages/ViewPosts.php
+++ src/Pages/ViewPosts.php
@@ -18,14 +18,14 @@ require_once __DIR__ . '/../SharedComponents/Header.php';
             <div class="blog-post mb-5 border border-3 p-3 rounded w-75">
                 <div class="d-flex justify-content-between w-100 align-items-center">
                     <h2 class="blog-title"><?php echo $post->title ?></h2>
-                    <div>Posted by <span class="fst-italic text-success text-decoration-underline"><?php echo $post->user ?> </span>on <?php echo $post->date ?></div>
+                    <div>Posted by <span class="fst-italic text-primary text-decoration-underline"><?php echo $post->user ?> </span>on <?php echo $post->date ?></div>
                 </div>
 
 
                 <div class="blog-content mb-4"><?php echo $post->blogContent ?></div>
                 <div class="blog-tags">
                     <?php foreach ($post->tags as $tag) : ?>
-                        <span class="tag rounded-pill bg-success p-1 ps-2 pe-2 text-white"><?php echo $tag; ?></span>
+                        <span class="tag rounded-pill bg-primary p-1 ps-2 pe-2 text-white"><?php echo $tag; ?></span>
                     <?php endforeach ?>
                 </div>
                 <br />
--- src/SharedComponents/Header.php
+++ src/SharedComponents/Header.php
@@ -14,7 +14,7 @@
 
 
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Blog</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
