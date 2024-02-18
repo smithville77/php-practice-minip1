ddd45ae4a3c38a4f0f06f3dea8801a9af14b69c4
Adam Smith <volcom_stone664@hotmail.com>
2024-02-18 11:32:49 +1100

    styled footer

--- src/Pages/ViewPosts.php
+++ src/Pages/ViewPosts.php
@@ -36,3 +36,7 @@ require_once __DIR__ . '/../SharedComponents/Header.php';
     </div>
 
 </div>
+<?php
+// include the header file
+require_once __DIR__ . '/../SharedComponents/Footer.php';
+?>
--- src/SharedComponents/Footer.php
+++ src/SharedComponents/Footer.php
@@ -1,5 +1,23 @@
+<?php
+
+$today = getdate();
+
+$currentYear = $today["year"];
+
+?>
+
 <footer>
 
+
+<div class="container">
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+    </ul>
+    <p class="text-center text-muted">© MyBlog inc, <?php echo $currentYear ?> </p>
+  </footer>
+</div>
 </footer>
 </body>
 
